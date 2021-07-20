dc6579302cb98450b32fc5ecdaf1407c4bd95157
leandro lima <leandro6666lima@gmail.com>
2021-07-20 17:10:10 -0300

    tela inicial 20/07 4

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-const express = require('express'); 
-const bodyParser = require('body-parser'); 
-
-
-
-const app = express(); 
-
-app.use(express.static(__dirname + '/Public')) 
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); 
-
-
-
-
-
-
-app.get('/', (req, res) => {
-
-    res.sendFile(__dirname + '/index.html') 
-});
-
-module.exports = app 
-=======
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
@@ -40,4 +15,3 @@ app.get('/' , (req, res) =>{
 app.use('/cadastros', rotaCadastros);
 
 module.exports = app
->>>>>>> 4bf14f46dad9c70d37ba6f591edfac31838f9424
