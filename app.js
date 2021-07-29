90fc61709c49c2dff960f1cf138fe6c665a22dec
Caetano Nascimento <caetanopark@gmail.com>
2021-07-29 16:52:18 -0300

    rotas feitas e links colocados

--- app.js
+++ app.js
@@ -14,19 +14,35 @@ app.get('/' , (req, res) =>{
 })
 
 app.get('/cadastro', (req, res) => {
-    res.sendFile(__dirname + '/public/paginas/cadastro.html')
+    res.sendFile(__dirname + '/public/paginas/cadastro.html');
 })
 
 app.get('/contato', (req, res) => {
-    res.sendFile(__dirname + '/public/paginas/fale_conosco.html')
+    res.sendFile(__dirname + '/public/paginas/fale_conosco.html');
 })
 
 app.get('/trabalhe_conel', (req, res) => {
-    res.sendFile(__dirname + '/public/paginas/trabalhe_conel.html')
+    res.sendFile(__dirname + '/public/paginas/trabalhe_conel.html');
 })
 
 app.get('/denuncia', (req, res) => {
-    res.sendFile(__dirname + '/public/paginas/denuncia.html')
+    res.sendFile(__dirname + '/public/paginas/denuncia.html');
+})
+
+app.get('/nossa_politica', (req, res) => {
+    res.sendFile(__dirname + '/public/paginas/missao_visao_valor.html');
+})
+
+app.get('/o_que_fazemos', (req, res) => {
+    res.sendFile(__dirname + '/public/paginas/o_que_fazemos.html');
+})
+
+app.get('/fique_por_dentro', (req,res) => {
+    res.sendFile(__dirname + '/public/paginas/fique_por_dentro.html');
+})
+
+app.get('/fornecedores', (req,res) => {
+    res.sendFile(__dirname + '/public/paginas/relacoes_com_fornecedores.html');
 })
 
 app.use('/cadastros', rotaCadastros);
