b92e6a1a84fd589f2a55743b4090753bde0e4645
Caetano Nascimento <caetanopark@gmail.com>
2021-08-12 11:29:25 -0300

    altereçoes

--- routes/trabalhe.js
+++ routes/trabalhe.js
@@ -13,15 +13,7 @@ const storeg = multer.diskStorage({
         cb(null,file.originalname)
     }
 })
-/*const fileFilter = (req, file, cb) =>{
 
-    if (file.mimetype === 'image/jpeg'  || file.mimetype === 'image/png' || file.mimetype === 'application/pdf'){
-   
-        cb(null, true);
-    }else{
-        cb(null, false)
-    }
-}*/
 const upload = multer({ 
     storage: storeg,
 });
