235f9883bdf83efa6468eef1c80ab797f6171188
Caetano Nascimento <caetanopark@gmail.com>
2021-08-18 13:34:22 -0300

    variaveis de ambiente

--- server_conel.js
+++ server_conel.js
@@ -3,4 +3,10 @@ const app = require('./app');
 const port = process.env.PORT || 2000;
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, ()=>{
+    console.log("rodando aqui" + JSON.stringify({"user": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "port": process.env.MYSQL_PORT}))
+});
