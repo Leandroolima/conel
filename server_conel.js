5ae71544beca053414b763d5e17a654f5e5d792f
Caetano Nascimento <caetanopark@gmail.com>
2021-08-20 12:30:19 -0300

    zoomm

--- server_conel.js
+++ server_conel.js
@@ -4,9 +4,5 @@ const port = process.env.PORT || 2000;
 const server = http.createServer(app);
 
 server.listen(port, ()=>{
-    console.log("rodando aqui" + JSON.stringify({"user": process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
-    "database": process.env.MYSQL_DATABASE,
-    "host": process.env.MYSQL_HOST,
-    "port": process.env.MYSQL_PORT}))
+
 });
